f8bc9f45b8394f2e7b4a1de3cb87c505187757e3
jianglihua <jianglihua@flashhold.com>
2022-12-19 17:40:32 +0800

    feat: 安装数据库Driver,配置数据库连接

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -7,4 +7,14 @@ export default {
     port: 7001,
     globalPrefix: '/api/v1',
   },
+  orm: {
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: '', // 数据库密码
+    database: 'midway', // 数据表
+    synchronize: true,
+    logging: false,
+  },
 } as MidwayConfig;
